2c7382b130f3aa97d2406daacc3c41b12aea61d1
Eric Ho <endonoho@gmail.com>
2020-01-09 15:23:17 -0800

    created Reply unit test

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,21 @@
+<?php
+// phpcs:ignoreFile
+namespace Tests\Unit;
+
+use App\User;
+use App\Reply;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReplyTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_has_an_owner()
+    {
+        $reply = factory(Reply::class)->create();
+
+        $this->assertInstanceOf(User::class, $reply->owner);
+    }
+}
