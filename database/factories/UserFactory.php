79d49c20e1a9c2cd842c7b3bc403c36f14102f71
Eric Ho <endonoho@gmail.com>
2020-01-09 10:18:10 -0800

    created Thread and Reply Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,10 +1,12 @@
 <?php
-
+// phpcs:ignoreFile
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
-use Faker\Generator as Faker;
+use App\Reply;
+use App\Thread;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +26,27 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'thread_id' => function () {
+            return factory(Thread::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'body' => $faker->paragraph
     ];
 });
