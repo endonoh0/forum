a39f34bee7707fa9c506028a4ccec31691d99bc5
Eric Ho <endonoho@gmail.com>
2020-01-09 14:02:20 -0800

    added path method

--- app/Thread.php
+++ app/Thread.php
@@ -1,10 +1,13 @@
 <?php
-
+// phpcs:ignoreFile
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    public function path()
+    {
+        return '/threads/' . $this->id;
+    }
 }
