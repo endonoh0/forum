fe72301e3024f3059125ebcd40891e79730538ca
Eric Ho <endonoho@gmail.com>
2020-01-11 17:02:25 -0800

    created URI to ThreadsController@index

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/threads', 'ThreadsController@index')->name('threads.index');
 Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show')->name('threads.show');
 Route::post('/threads', 'ThreadsController@store');
-
+Route::get('/threads/{channel}', 'ThreadsController@index')->name('threads.index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
