fcebdf98ee633b132ae50384db4b9442abff59bb
Eric Ho <endonoho@gmail.com>
2020-01-11 19:01:41 -0800

    added create view to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,10 @@
                             <a class="nav-link" href="/threads">All Threads</a>
                         </li>
 
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/threads/create">New Thread</a>
+                        </li>
+
                         <li class="nav-item dropdown">
                           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Channels <span class="caret"></span></a>
