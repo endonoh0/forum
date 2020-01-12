52ea2ffa46710a3e27c871d665c1a22e38cea1ef
Eric Ho <endonoho@gmail.com>
2020-01-12 14:05:52 -0800

    changed provider to load view, then query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::share('channels', Channel::all());
+        \View::composer('*', function ($view) {
+            $view->with('channels', Channel::all());
+        });
     }
 }
