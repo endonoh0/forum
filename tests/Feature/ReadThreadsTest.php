268be293573d73c947c345961250c661fa679e4c
Eric Ho <endonoho@gmail.com>
2020-01-12 14:06:49 -0800

    added filter by username feature test

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -2,6 +2,7 @@
 // phpcs:ignoreFile
 namespace Tests\Feature;
 
+use App\User;
 use App\Reply;
 use App\Thread;
 use App\Channel;
@@ -53,5 +54,18 @@ class ThreadsTest extends TestCase
         $this->get('/threads/' . $channel->slug)
             ->assertSee($threadInChannel->title)
             ->assertDontSee($threadNotInChannel->title);
+    }
+
+    /** @test */
+    public function a_user_can_filter_threads_by_any_username()
+    {
+        $this->signIn(create(User::class, ['name' => 'JohnDoe']));
+
+        $threadByJohn = create(Thread::class, ['user_id' => auth()->id()]);
+        $threadNotByJohn = create(Thread::class);
+
+        $this->get('/threads?by=JohnDoe')
+            ->assertSee($threadByJohn->title)
+            ->assertDontSee($threadNotByJohn->title);
     }
 }
