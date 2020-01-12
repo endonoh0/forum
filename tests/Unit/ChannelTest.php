aead76e8940b18e2d750b1a84de058300bf6d722
Eric Ho <endonoho@gmail.com>
2020-01-11 17:03:52 -0800

    added unit test for channel

---
+++ tests/Unit/ChannelTest.php
@@ -0,0 +1,22 @@
+<?php
+// phpcs:ignoreFile
+namespace Tests\Feature;
+
+use App\Thread;
+use App\Channel;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ChannelTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_channel_consists_of_threads()
+    {
+        $channel = create(Channel::class);
+        $thread = create(Thread::class, ['channel_id' => $channel->id]);
+
+        $this->assertTrue($channel->threads->contains($thread));
+    }
+}
