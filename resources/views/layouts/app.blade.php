d2f51ee8380f357d2a99b7fda49c045d656e4666
Eric Ho <endonoho@gmail.com>
2020-01-11 17:00:22 -0800

    added dropdown menu to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+
+                <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -33,7 +36,23 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li><a href="/threads">All Threads</a></li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/threads">All Threads</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Channels <span class="caret"></span></a>
+
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                @foreach (App\Channel::all() as $channel)
+                                    <div>
+                                        <a class="dropdown-item" href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a>
+                                    </div>
+                                    <div class="dropdown-divider"></div>
+                                @endforeach
+                          </div>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
