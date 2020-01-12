43510064434bd00e36a147946398df68419ba9e5
Eric Ho <endonoho@gmail.com>
2020-01-11 16:59:35 -0800

    updated index method

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -19,12 +19,16 @@ class ThreadsController extends Controller
 
     /**
      * Display a listing of the resource.
-     *
+     * @param Channel $channel
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Channel $channel)
     {
-        $threads = Thread::latest()->get();
+        if ($channel->exists) {
+            $threads = $channel->threads()->latest()->get();
+        } else {
+            $threads = Thread::latest()->get();
+        }
 
         return view('threads.index', compact('threads'));
     }
