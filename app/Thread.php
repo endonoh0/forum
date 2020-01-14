f7c1d84010fe15b9c57cebf493d69357205a215f
Eric Ho <endonoho@gmail.com>
2020-01-14 11:05:38 -0800

    added global query scope for reply count

--- app/Thread.php
+++ app/Thread.php
@@ -13,6 +13,15 @@ class Thread extends Model
      */
     protected $guarded = [];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
     /**
      * Get a string path for the thread.
      *
