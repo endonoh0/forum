a1422dc0af10059dfb8d9b3d72dc08f886568488
Eric Ho <endonoho@gmail.com>
2020-01-14 11:22:07 -0800

    updated meta information panel

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,18 +12,19 @@
                     {{ $thread->title }}
                 </div>
                 <div class="card-body">
-                    <div class="body">
-                        {{ $thread->body }}
-                    </div>
+                    {{ $thread->body }}
                 </div>
             </div>
 
             <br>
 
             {{-- Reply --}}
-            @foreach ($thread->replies as $reply)
+
+            @foreach ($replies as $reply)
                 @include ('threads.reply')
             @endforeach
+
+            {{ $replies->links() }}
 
             {{-- Add Form Reply --}}
             @if (auth()->check())
@@ -45,6 +46,21 @@
                 <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
             @endif
         </div>
+
+        {{-- Meta Information --}}
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p>
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by
+                        {{-- link to profile --}}
+                        <a href="#">{{ $thread->creator->name }}</a>, and currently
+                        has {{ $thread->replies_count }} {{ Str::plural('comment', $thread->replies_count) }}.
+                    </p>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
