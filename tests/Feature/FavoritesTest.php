4d7886e66482a4959b02cc3b41c53a5193f76233
Eric Ho <endonoho@gmail.com>
2020-01-14 13:59:10 -0800

    added users can favorite reply feature

---
+++ tests/Feature/FavoritesTest.php
@@ -0,0 +1,48 @@
+<?php
+// phpcs:ignoreFile
+namespace Tests\Feature;
+
+use App\Reply;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class FavoritesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function guests_can_not_favorite_anything()
+    {
+        $this->post('replies/1/favorites')
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        $this->post('replies/' . $reply->id . '/favorites');
+
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    /** @test */
+    public function an_authenticated_user_may_only_favorite_a_reply_once()
+    {
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        try {
+            $this->post('replies/' . $reply->id . '/favorites');
+            $this->post('replies/' . $reply->id . '/favorites');
+        } catch (\Exception $e) {
+            $this->fail('Did not expect to insert the same record set twice.');
+        }
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
