997b87f85508d95d9fc64a4e5797888110b8e692
Eric Ho <endonoho@gmail.com>
2020-01-15 12:16:02 -0800

    updated to cascade delete thread and replies associated

--- app/Thread.php
+++ app/Thread.php
@@ -32,6 +32,10 @@ class Thread extends Model
         static::addGlobalScope('replyCount', function ($builder) {
             $builder->withCount('replies');
         });
+
+        static::deleting(function ($thread) {
+            $thread->replies()->delete();
+        });
     }
 
     /**
