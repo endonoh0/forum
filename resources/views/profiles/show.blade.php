5e2719ee563dd3d3accdbda31e30cef17642dea3
Eric Ho <endonoho@gmail.com>
2020-01-15 12:16:55 -0800

    updated user's profile view

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,35 +2,40 @@
 
 @section ('content')
     <div class="container">
-        <div class="card-header">
-            {{-- Profile header --}}
-            <h1>
-                {{ $profileUser->name }}
-                <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-            </h1>
-        </div>
-
-        <br>
-        {{-- All profile user's thread --}}
-        @foreach ($threads as $thread)
-            <div class="card">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
                 <div class="card-header">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="#">{{ $thread->creator->name }}</a> posted:
-                            {{ $thread->title }}
-                        </span>
-                        <span>{{ $thread->created_at->diffForHumans() }}</span>
+                    {{-- Profile header --}}
+                    <h2>
+                        {{ $profileUser->name }}
+                        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+                    </h2>
+                </div>
+
+                <br>
+                {{-- All profile user's thread --}}
+                @foreach ($threads as $thread)
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="level">
+                                <span class="flex">
+                                    <a href="#">{{ $thread->creator->name }}</a> posted:
+                                    {{ $thread->title }}
+                                </span>
+                                <span>{{ $thread->created_at->diffForHumans() }}</span>
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            {{ $thread->body }}
+                        </div>
                     </div>
-                </div>
+                    <br>
+                @endforeach
 
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
+                {{ $threads->links() }}
+
             </div>
-            <br>
-        @endforeach
-
-        {{ $threads->links() }}
+        </div>
     </div>
 @endsection
