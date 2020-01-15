0626f6ed542c15005fa645625d8d0b2e590adc33
Eric Ho <endonoho@gmail.com>
2020-01-15 12:17:39 -0800

    added delete request to route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index')->name('threads.index');
 Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show')->name('threads.show');
+Route::delete('/threads/{channel}/{thread}', 'ThreadsController@destroy');
 Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{channel}', 'ThreadsController@index')->name('threads.index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
