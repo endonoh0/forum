17882bd99ca75668a5e058fa13d8cc581bae4a28
Eric Ho <endonoho@gmail.com>
2020-01-16 10:23:14 -0800

    updated authorization to delete thread request

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -87,6 +87,8 @@ class ThreadsController extends Controller
 
     public function destroy(Channel $channel, Thread $thread)
     {
+        $this->authorize('update', $thread);
+
         if ($thread->user_id != auth()->id()) {
             abort(403, 'You do not have permission to do this.');
         }
