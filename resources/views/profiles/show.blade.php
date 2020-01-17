da368321a6ddd2fe9b0d79a1802c1fdfb7cfb741
Eric Ho <endonoho@gmail.com>
2020-01-16 19:18:30 -0800

    updated activity feed in profile user view

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,33 +8,18 @@
                     {{-- Profile header --}}
                     <h2>
                         {{ $profileUser->name }}
-                        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
                     </h2>
                 </div>
 
                 <br>
-                {{-- All profile user's thread --}}
-                @foreach ($threads as $thread)
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="level">
-                                <span class="flex">
-                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                </span>
-                                <span>{{ $thread->created_at->diffForHumans() }}</span>
-                            </div>
-                        </div>
-
-                        <div class="card-body">
-                            {{ $thread->body }}
-                        </div>
-                    </div>
+                {{-- Profile user's threads and replies --}}
+                @foreach ($activities as $date => $activity)
+                    <h3 class="card-header">{{ $date }}</h3>
                     <br>
+                    @foreach ($activity as $record)
+                        @include ("profiles.activities.{$record->type}", ['activity' => $record])
+                    @endforeach
                 @endforeach
-
-                {{ $threads->links() }}
-
             </div>
         </div>
     </div>
