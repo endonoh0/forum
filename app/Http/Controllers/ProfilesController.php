db537b440b3bc1137d2b5954d8204408c70c3cb2
Eric Ho <endonoho@gmail.com>
2020-01-17 10:58:49 -0800

    extracted Eloquent query from ProfilesController into Activty model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Activity;
 
 class ProfilesController extends Controller
 {
@@ -16,14 +17,7 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $this->getActivity($user)
+            'activities' => Activity::feed($user) // $this->getActivity($user)
         ]);
-    }
-
-    public function getActivity(User $user)
-    {
-        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
