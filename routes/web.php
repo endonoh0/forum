03e2ded2d71ffe653bb59cac9e7562eace61d15c
Eric Ho <endonoho@gmail.com>
2020-01-20 11:11:39 -0800

    updated delete favorites route

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,6 @@ Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::patch('replies/{reply}', 'RepliesController@update');
 Route::delete('replies/{reply}', 'RepliesController@destroy');
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+Route::delete('/replies/{reply}/favorites', 'FavoritesController@destroy');
 
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
