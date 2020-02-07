5d0fda1aa8ef54dcd97b6ececb3173d952f8eaa4
Eric Ho <endonoho@gmail.com>
2020-02-07 13:36:07 -0800

    added users can subscribe to threads unit test

---
+++ database/migrations/2020_02_07_205728_create_thread_subscriptions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thread_subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('thread_id');
+            $table->timestamps();
+            $table->unique(['user_id', 'thread_id']);
+
+            $table->foreign('thread_id')
+                ->references('id')
+                ->on('threads')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thread_subscriptions');
+    }
+}
