6e295001ef8637586d9a8dabf215829fb8800f10
Eric Ho <endonoho@gmail.com>
2020-02-09 13:51:29 -0800

    added a user can subscribe to threads feature test

---
+++ tests/Feature/SubcribeToThreadsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_subscribe_to_threads()
+    {
+        $this->withoutExceptionHandling();
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        // when the user subscribes to a thread
+        $this->post($thread->path() . '/subscriptions');
+
+        // then, each time a new reply is left ...
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here.'
+        ]);
+
+        // a notfication should be prepared for the user
+        // $this->assertCount(1, auth()->user()->notifications);
+    }
+}
