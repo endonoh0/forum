472470350d50677179fbc62743a9fd112a760e3e
Eric Ho <endonoho@gmail.com>
2020-02-26 12:03:21 -0800

    added spamfree

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -117,6 +117,7 @@ return [
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute format is invalid.',
     'uuid' => 'The :attribute must be a valid UUID.',
+    'spamfree' => 'The :attribute contains spam',
 
     /*
     |--------------------------------------------------------------------------
