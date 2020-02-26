e989cd682a4de51bd78aaa1dd7cdb92f2b2ac9f9
Eric Ho <endonoho@gmail.com>
2020-02-26 11:05:23 -0800

    updated to catch spam and render feedback to user

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -65,8 +65,12 @@
 
         methods: {
             update() {
-                axios.patch('/replies/' + this.data.id, {
-                    body: this.body
+                axios.patch(
+                    '/replies/' + this.data.id, {
+                        body: this.body
+                })
+                .catch(error => {
+                    flash(error.response.data, 'danger');
                 });
 
                 this.editing = false;
