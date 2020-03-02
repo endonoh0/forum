0fa4e7660e30063ca5a36b9cad21b38ebe0a1636
Eric Ho <endonoho@gmail.com>
2020-03-02 10:48:34 -0800

    updated HTTP responses

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -54,6 +55,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ValidationException) {
+            if ($request->expectsJson()) {
+                return response('Sorry, validation failed.', 422);
+            }
+        }
+
+        if ($exception instanceof ThrottleException) {
+            return response('You are posting too frequently.', 429);
+        }
+
         return parent::render($request, $exception);
     }
 }
