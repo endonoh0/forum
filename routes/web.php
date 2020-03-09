1d2053341ac62b6f62c7a3668898db1d953b04e5
Eric Ho <endonoho@gmail.com>
2020-03-09 07:29:44 -0700

    Added web routes for avatar

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,4 @@ Route::get('/profiles/{user}/notifications/', 'UserNotificationsController@index
 Route::delete('/profiles/{user}/notifications/{notification}', 'UserNotificationsController@destroy');
 
 Route::get('api/users', 'Api\UsersController@index');
+Route::post('api/users/{user}/avatar', 'Api\UserAvatarController@store')->middleware('auth');
