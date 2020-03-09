1a1075ea71c8637de43023f7a8da876a1bcdfefa
Eric Ho <endonoho@gmail.com>
2020-03-08 19:31:08 -0700

    Updated thread's creator username to index view

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,39 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @forelse ($threads as $thread)
-                <article class="card">
-                    <div class="card-header level">
-                        {{-- Thread title --}}
-                        <h4 class="flex">
-                            <a href="{{ $thread->path() }}">
-                                @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                    <strong>
-                                        {{ $thread->title }}
-                                    </strong>
-                                @else
-                                    {{ $thread->title }}
-                                @endif
-                            </a>
-                        </h4>
-                        {{-- Reply count --}}
-                        <a href="{{ $thread->path() }}">
-                            {{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}
-                        </a>
-                    </div>
-                    {{-- Thread body --}}
-                    <div class="card-body">
-                        {{ $thread->body }}
-                    </div>
-                </article>
-             <br>
-             @empty
-                <p>There are no relevant results at this time.</p>
-            @endforelse
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @include ('threads._list')
+
+                {{-- Render and paginate threads --}}
+                {{ $threads->render() }}
+            </div>
         </div>
     </div>
-</div>
 @endsection
