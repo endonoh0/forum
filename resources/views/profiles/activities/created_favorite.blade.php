4bf3e027d0c24c7755f2657ea8d000e9355c990e
Eric Ho <endonoho@gmail.com>
2020-04-21 13:48:57 -0700

    deleted commented code

--- resources/views/profiles/activities/created_favorite.blade.php
+++ resources/views/profiles/activities/created_favorite.blade.php
@@ -3,7 +3,6 @@
         <a href="{{ $activity->subject->favorited->path() }}">
             {{ $profileUser->name }} favorited a reply.
         </a>
-        {{-- <a href="{{ $activity->subject->thread->path() }}">"{{ $activity->subject->thread->title }}"</a> --}}
     @endslot
 
     @slot('body')
