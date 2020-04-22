55d5c93726beb12f81bea81d32cd7ee3a4b39342
Eric Ho <endonoho@gmail.com>
2020-04-22 11:23:24 -0700

    Updated doc blocks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,15 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
-
- window.events = new Vue();
-
- window.flash = function (message, level = 'success') {
-     window.events.$emit('flash', { message, level });
- };
-
 
 window.Vue = require('vue');
 
 import InstantSearch from 'vue-instantsearch';
+
 Vue.use(InstantSearch);
 
 let authorizations = require('./authorizations');
@@ -26,12 +26,35 @@ Vue.prototype.authorize = function (...params) {
 
 Vue.prototype.signedIn = window.App.signedIn;
 
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/Flash.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
 Vue.component('flash', require('./components/Flash.vue').default);
 Vue.component('paginator', require('./components/Paginator.vue').default);
 Vue.component('user-notifications', require('./components/UserNotifications.vue').default);
 Vue.component('avatar-form', require('./components/AvatarForm.vue').default);
 Vue.component('wysiwyg', require('./components/Wysiwyg.vue').default);
 Vue.component('thread-view', require('./pages/Thread.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+window.events = new Vue();
+
+ window.flash = function (message, level = 'success') {
+     window.events.$emit('flash', { message, level });
+ };
 
 const app = new Vue({
     el: '#app'
